7a890ff54c1b16de637b6b0d7de0d29c90e45eb0
utsav-patel <utsavpatel@gridkey.in>
2025-10-09 11:56:40 +0530

    Build 3

--- src/components/Ninedotleftref.tsx
+++ src/components/Ninedotleftref.tsx
@@ -6,14 +6,9 @@ const Ninedotleftref = () => {
   return (
     <div
       ref={nineDotsLeftRef}
-      className="w-full md:w-1/2 flex items-center justify-center absolute opacity-0 top-1/2 -translate-y-1/2 px-4"
+      className="gradiantBg w-full md:w-1/2 flex items-center justify-center absolute opacity-0 top-1/2 -translate-y-1/2 px-4"
     >
-      <div
-        className="flex flex-col w-full md:w-[80%] gap-4 rounded-full bg-[#000f13]"
-        style={{
-          boxShadow: "0 0 1000px 200px #000f13",
-        }}
-      >
+      <div className="flex flex-col w-full md:w-[80%] gap-4 rounded-full">
         <div className="border border-grey-700  rounded-[8px] p-2 w-fit">
           <p className="text-grey-500 uppercase text-">tech tools</p>
         </div>
